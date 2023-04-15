5d7fae4e3f9ef4e1ce96f33e3123535eee1fc8ff
Gabriel Dias <42484610+gddeazevedo@users.noreply.github.com>
2023-04-15 18:26:04 -0300

    moving main to src folder

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include "./avl_tree/avl_tree.h"
+
+void main() {
+  Tree* t = newTree();
+  tree_insert(t, 'g');
+  tree_insert(t, 'b');
+  tree_insert(t, 'm');
+  tree_insert(t, 'a');
+  tree_insert(t, 'c');
+  tree_insert(t, 'j');
+  tree_insert(t, 'o');
+  tree_insert(t, 'n');
+  tree_remove(t, 'g');
+
+  tree_preorder_walk(t->root);
+}
